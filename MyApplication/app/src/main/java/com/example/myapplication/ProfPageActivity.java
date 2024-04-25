652285cc75130e04b48793fa09523ddabaebb61f
Yujing Zhang <u7671098@anu.edu.au>
2024-04-25 14:45:57 +1000

    draw layout of prof_page

--- MyApplication/app/src/main/java/com/example/myapplication/ProfPageActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ProfPageActivity.java
@@ -2,12 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;//import for display in TextView
+import android.widget.ImageView;
 
 public class ProfPageActivity extends AppCompatActivity {
-
+    private TextView buildingInfo;
+    //private ImageView buildingImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +19,20 @@ public class ProfPageActivity extends AppCompatActivity {
 
         Button followButton = null;
         Button fireAlarmNoti=null;
+        //--yujing--
+        //buildingImage = findViewById(R.id.buildingImage);
+        buildingInfo = findViewById(R.id.buildingInfo);
+        String[] building = {"1234 Main St", "1990", "30", "Concrete"};//string[] sample for display test
+
+        String infoText = "Addr: " + building[0] + "\n" +
+                building[1] + "\n" +
+                building[2] + "\n" +
+                building[3] + "\n" +
+                building[4] + "\n" +
+                building[5];
+        buildingInfo.setText(infoText);
+
+        //----yujing--end-----
         followButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,4 +47,4 @@ public class ProfPageActivity extends AppCompatActivity {
             }
         });
     }
-}
+
