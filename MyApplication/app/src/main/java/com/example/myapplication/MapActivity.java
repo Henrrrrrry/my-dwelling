f0df5d43163298878c9494f51411bbfa325d440a
Hongyu Li <u7776180@anu.eadu.au>
2024-05-10 04:20:17 +1000

    implemented marker info click function, added animation of repair button, implemented moving camera when click search button, created a toast when no search result

--- MyApplication/app/src/main/java/com/example/myapplication/MapActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MapActivity.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,6 +21,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListPopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +36,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,11 +45,12 @@ import helper_classes_and_methods.Dwelling;
 import helper_classes_and_methods.User;
 
 public class MapActivity extends BaseActivity implements OnMapReadyCallback {
-    //    create a dwelling entity test: Dwelling dwelling= new Dwelling();
+
     private GoogleMap Mmap;
     private Location location;
     private EditText userInput;
     private ListPopupWindow listPopupWindow;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +59,8 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
         //Get a handle to the fragment and register the callback. Means loading map I guess
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        user = (User) getIntent().getExtras().getSerializable("USER");
 
-        User user = (User) getIntent().getExtras().getSerializable("USER");
 
         //search_text
         userInput = findViewById(R.id.search_text);
@@ -85,12 +93,16 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
             public void onClick(View v) {
                 // get user's input
                 String inputStr = userInput.getText().toString();
+
                 Dwelling searchDwelling = dataLoader.getBTree().get(inputStr);
-                Intent intent = new Intent(MapActivity.this, ProfPageActivity.class);
-                intent.putExtra("Dwelling",searchDwelling);
-                intent.putExtra("User",user);
-                startActivity(intent);
-                finish();
+                if (searchDwelling!=null){
+                    LatLng latLng = new LatLng(searchDwelling.getLocation().getLat(),  searchDwelling.getLocation().getLng());
+                    Mmap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+                }
+                else {
+                    Toast.makeText(MapActivity.this, "Address not found.", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
@@ -110,25 +122,45 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
         listPopupWindow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                userInput.setText(addressList.get(position));
+//                userInput.setText(addressList.get(position));
+//                listPopupWindow.dismiss();
+                String addr = addressList.get(position);
+                userInput.setText(addr);
                 listPopupWindow.dismiss();
+                Dwelling dwelling = dataLoader.getBTree().get(addr);
+                LatLng latLng = new LatLng(dwelling.getLocation().getLat(), dwelling.getLocation().getLng());
+                Mmap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+
             }
         });
         listPopupWindow.show();
     }
 
     //    When the map is loaded this method would be called.
+    @SuppressLint("PotentialBehaviorOverride")
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         Mmap = googleMap;
         addMarkers();
         viewCurrentLocation();//Ask for users current location
-        //LatLng Canberra = new LatLng(-35.2966, 149.1290); seeCurrent(Canberra);//for test
-        Mmap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        Mmap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+
             @Override
-            public boolean onMarkerClick(@NonNull Marker marker) {
-                marker.showInfoWindow();
-                return true;
+            public void onInfoWindowClick(@NonNull Marker marker) {
+                Dwelling searchDwelling = dataLoader.getBTree().get((String) marker.getTag());
+                if (searchDwelling!=null){
+                    Intent intent = new Intent(MapActivity.this, ProfPageActivity.class);
+                    intent.putExtra("Dwelling", searchDwelling);
+                    intent.putExtra("User", user);
+                            startActivity(intent);
+                    LatLng latLng = new LatLng(searchDwelling.getLocation().getLat(),  searchDwelling.getLocation().getLng());
+                    Mmap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 20));
+
+
+                }
+                else {
+                    Toast.makeText(MapActivity.this, "Address not found.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -174,23 +206,25 @@ public class MapActivity extends BaseActivity implements OnMapReadyCallback {
     }
     //set camera and zoom in default 15
     private void seeCurrent(LatLng current) {
-        Mmap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 15));
+        Mmap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 14));
     }
     private void addMarkers() { //markers all dwelling on the initial map
         for (Dwelling d : dataLoader.getBTree().getDwellings()) {
             addOneMarker(new LatLng(d.getLocation().getLat(),
-                    d.getLocation().getLng()), d.getSeismicRating());
-//            addOneMarker(new LatLng(d.getLocation().getLat(),
-//                    d.getLocation().getLng()), 5);
+                    d.getLocation().getLng()), d.getSeismicRating(),
+                    d.getAddress());
         }
     }
-    private void addOneMarker(LatLng coordinates,int colorType){ //add single mark with special color
-        Mmap.addMarker(new MarkerOptions()
+    private void addOneMarker(LatLng coordinates,int colorType,String address){ //add single mark with special color
+        Marker marker=
+                Mmap.addMarker(new MarkerOptions()
                 .position(coordinates)
                 .icon(BitmapDescriptorFactory.defaultMarker(getHueFromColorType(colorType)))
-                .title("SR: " + colorType));//show SR level 1-10
+                .title("SR: " + colorType+", click to see more detail"));//show SR level 1-10
+        marker.setTag(address);
     }
     private float getHueFromColorType(int colorType) {
         return 12f * (colorType - 1); // 1-red,... ,10-green: SR level color
     }
+
 }
--- MyApplication/app/src/main/java/com/example/myapplication/ProfPageActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ProfPageActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,6 +23,7 @@ import java.util.ArrayList;
 import helper_classes_and_methods.BuildingMaterial;
 import helper_classes_and_methods.Dwelling;
 import helper_classes_and_methods.Observer;
+import helper_classes_and_methods.RepairState;
 import helper_classes_and_methods.User;
 import helper_classes_and_methods.StorageFactory;
 import helper_classes_and_methods.StorageHandler;
@@ -29,19 +32,15 @@ import helper_classes_and_methods.StorageHandler;
 public class ProfPageActivity extends BaseActivity {
     //                test case create channel ID
     public static final String CHANNEL_ID = "uniqueChannelId";
-     ArrayList<Observer> observers1= new ArrayList<>();
+//     ArrayList<Observer> observers1= new ArrayList<>();
     private StorageHandler storageHandler;
 
     //private ImageView buildingImage;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        test case create fake info&channel
-//        User maintainer = new User("mok3@163.com","666666",true,"Xinfei");
-//        Dwelling.Location locationTest = new Dwelling.Location(-35.255290,149.145020);
-//        Dwelling dwelling= new Dwelling("41 davenport St", LocalDate.of(1948, Month.OCTOBER, 1),BuildingMaterial.WOOD,observers1,maintainer,locationTest);
-//        User user1 = new User("a2546556102@gmail.com", "123456",false,"Henry");
-//        observers1.add(user1);
+
         createNotificationChannel();
         // Initialize StorageHandler
         storageHandler = StorageFactory.getStorageHandler(this, StorageFactory.HandlerType.FIRE_ALARM);
@@ -53,22 +52,23 @@ public class ProfPageActivity extends BaseActivity {
 
         Button followButton = findViewById(R.id.followButton);
         Button fireAlarmNoti = findViewById(R.id.fireAlarmButton);
+
+
         User user = (User) getIntent().getExtras().getSerializable("User");
         if (!user.isMaintainer()) fireAlarmNoti.setEnabled(false);
         Button backButton = findViewById(R.id.backButton);
         TextView buildingInfo = findViewById(R.id.buildingInfo);
         TextView buildingTitle= findViewById(R.id.address);
         //address
-        //TODO: this is test data reply with real data
+
         Dwelling searchDwelling = (Dwelling) getIntent().getExtras().getSerializable("Dwelling");
-//        String[] building = {
-//                searchDwelling.getAddress(),
-//                String.valueOf(searchDwelling.getSeismicRating()),
-//                String.valueOf(searchDwelling.isFireAlarm()),
-//                String.valueOf(searchDwelling.getConstructionDate()),
-//                String.valueOf(searchDwelling.getBuildingMaterial())
-//        };
-        //String[] building = {"1234 Main St","7","True", "1990", "30", "Concrete"};//string[] sample for display test
+
+        Button repairButton = findViewById(R.id.repairButton);
+        if (!(searchDwelling.needsRepair())){
+            repairButton.setEnabled(false);
+            repairButton.setText("Don't need Repair");
+        }
+
 
         String infoText = //"Address: " + searchDwelling.getAddress() + "\n" +
                 "Seismic Rating: "+searchDwelling.getSeismicRating() + "\n" +
@@ -109,7 +109,7 @@ public class ProfPageActivity extends BaseActivity {
 //                    tell users they have unfollowed and next time click on the button will follow again
                     followButton.setBackgroundColor(Color.rgb(0,0,128));
                     followButton.setText("Follow");
-                    System.out.println(searchDwelling.getObservers());
+//                    System.out.println(searchDwelling.getObservers());
                     Toast.makeText(getApplicationContext(),"you have unfollowed",Toast.LENGTH_SHORT).show();
 
                 }
@@ -120,7 +120,7 @@ public class ProfPageActivity extends BaseActivity {
 //                    tell users they have followed and next time click on the button will unfollow
                     followButton.setBackgroundColor(Color.rgb(128,128,128));
                     followButton.setText("Unfollow");
-                    System.out.println(searchDwelling.getObservers());
+//                    System.out.println(searchDwelling.getObservers());
                     Toast.makeText(getApplicationContext(),"you have followed",Toast.LENGTH_SHORT).show();
                 }
 
@@ -146,14 +146,42 @@ public class ProfPageActivity extends BaseActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent backIntent = new Intent(ProfPageActivity.this,MapActivity.class);
-                backIntent.putExtra("USER",user);
-                startActivity(backIntent);
+                finish();
+            }
+        });
+        repairButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Thank you for your maintenance",Toast.LENGTH_SHORT).show();
+                repairButton.setEnabled(false);
+
+                Animation fadeOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+                // start animation
+                repairButton.startAnimation(fadeOut);
+
+                // set animation
+                fadeOut.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                        repairButton.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
             }
         });
 
     }
-//    create a new channel, not sure should it be created in login activity or here
+//    create a new channel
     private void createNotificationChannel() {
         CharSequence name = "NotificationChannel";
         String description = "This is a channel for notification, with this the notification can post in noti bar";
